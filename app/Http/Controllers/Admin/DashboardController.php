e29c1ad73314641c4f821b4ab93c4dd3f6f25446
sugeng20 <sugeng.wanantara@gmail.com>
2023-06-28 14:39:53 +0700

    dashboard admin

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Faq;
+use App\Models\Jabatan;
+use App\Models\Kelas;
+use App\Models\Kreasi;
+use App\Models\MataPelajaran;
+use App\Models\Sekolah;
+use App\Models\Unduhan;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -11,6 +18,13 @@ class DashboardController extends Controller
     public function index()
     {
         $data['guru'] = User::where('role', 'GURU')->count();
+        $data['kelas'] = Kelas::count();
+        $data['sekolah'] = Sekolah::count();
+        $data['faq'] = Faq::count();
+        $data['jabatan'] = Jabatan::count();
+        $data['mata-pelajaran'] = MataPelajaran::count();
+        $data['unduhan'] = Unduhan::count();
+        $data['wadah-kreasi'] = Kreasi::count();
         return view('pages.admin.dashboard.index', compact('data'));
     }
 }
--- resources/views/pages/admin/dashboard/index.blade.php
+++ resources/views/pages/admin/dashboard/index.blade.php
@@ -30,7 +30,7 @@ Dashboard
 
     <div class="row">
         <div class="col-md-3">
-            <a href="#">
+            <a href="{{ route('guru.index') }}">
                 <div class="card report-card">
                     <div class="card-body">
                         <div class="row d-flex justify-content-center">
@@ -49,6 +49,167 @@ Dashboard
                                         <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
                                         <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
                                     </svg>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end card-body-->
+                </div>
+            </a>
+            <!--end card-->
+        </div>
+        <!--end col-->
+        <div class="col-md-3">
+            <a href="{{ route('kelas.index') }}">
+                <div class="card report-card">
+                    <div class="card-body">
+                        <div class="row d-flex justify-content-center">
+                            <div class="col">
+                                <p class="text-dark mb-1 fw-semibold">Kelas</p>
+                                <h4 class="font-22 fw-bold">{{ $data['kelas'] }}</h4>
+                            </div>
+                            <div class="col-auto align-self-center">
+                                <div
+                                    class="bg-light-alt d-flex justify-content-center align-items-center thumb-md  rounded-circle">
+                                    <i class="ti ti-archive menu-icon"></i><span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end card-body-->
+                </div>
+            </a>
+            <!--end card-->
+        </div>
+        <!--end col-->
+        <div class="col-md-3">
+            <a href="{{ route('sekolah.index') }}">
+                <div class="card report-card">
+                    <div class="card-body">
+                        <div class="row d-flex justify-content-center">
+                            <div class="col">
+                                <p class="text-dark mb-1 fw-semibold">Sekolah</p>
+                                <h4 class="font-22 fw-bold">{{ $data['sekolah'] }}</h4>
+                            </div>
+                            <div class="col-auto align-self-center">
+                                <div
+                                    class="bg-light-alt d-flex justify-content-center align-items-center thumb-md  rounded-circle">
+                                    <i class="ti ti-school menu-icon"></i><span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end card-body-->
+                </div>
+            </a>
+            <!--end card-->
+        </div>
+        <!--end col-->
+        <div class="col-md-3">
+            <a href="{{ route('faq.index') }}">
+                <div class="card report-card">
+                    <div class="card-body">
+                        <div class="row d-flex justify-content-center">
+                            <div class="col">
+                                <p class="text-dark mb-1 fw-semibold">FAQ</p>
+                                <h4 class="font-22 fw-bold">{{ $data['faq'] }}</h4>
+                            </div>
+                            <div class="col-auto align-self-center">
+                                <div
+                                    class="bg-light-alt d-flex justify-content-center align-items-center thumb-md  rounded-circle">
+                                    <i class="ti ti-question-mark menu-icon"></i><span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end card-body-->
+                </div>
+            </a>
+            <!--end card-->
+        </div>
+        <!--end col-->
+        <div class="col-md-3">
+            <a href="{{ route('jabatan.index') }}">
+                <div class="card report-card">
+                    <div class="card-body">
+                        <div class="row d-flex justify-content-center">
+                            <div class="col">
+                                <p class="text-dark mb-1 fw-semibold">Jabatan</p>
+                                <h4 class="font-22 fw-bold">{{ $data['jabatan'] }}</h4>
+                            </div>
+                            <div class="col-auto align-self-center">
+                                <div
+                                    class="bg-light-alt d-flex justify-content-center align-items-center thumb-md  rounded-circle">
+                                    <i class="ti ti-star menu-icon"></i><span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end card-body-->
+                </div>
+            </a>
+            <!--end card-->
+        </div>
+        <!--end col-->
+        <div class="col-md-3">
+            <a href="{{ route('mata-pelajaran.index') }}">
+                <div class="card report-card">
+                    <div class="card-body">
+                        <div class="row d-flex justify-content-center">
+                            <div class="col">
+                                <p class="text-dark mb-1 fw-semibold">Mata Pelajaran</p>
+                                <h4 class="font-22 fw-bold">{{ $data['mata-pelajaran'] }}</h4>
+                            </div>
+                            <div class="col-auto align-self-center">
+                                <div
+                                    class="bg-light-alt d-flex justify-content-center align-items-center thumb-md  rounded-circle">
+                                    <i class="ti ti-archive menu-icon"></i><span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end card-body-->
+                </div>
+            </a>
+            <!--end card-->
+        </div>
+        <!--end col-->
+        <div class="col-md-3">
+            <a href="{{ route('unduhan.index') }}">
+                <div class="card report-card">
+                    <div class="card-body">
+                        <div class="row d-flex justify-content-center">
+                            <div class="col">
+                                <p class="text-dark mb-1 fw-semibold">Unduhan</p>
+                                <h4 class="font-22 fw-bold">{{ $data['unduhan'] }}</h4>
+                            </div>
+                            <div class="col-auto align-self-center">
+                                <div
+                                    class="bg-light-alt d-flex justify-content-center align-items-center thumb-md  rounded-circle">
+                                    <i class="ti ti-download menu-icon"></i><span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end card-body-->
+                </div>
+            </a>
+            <!--end card-->
+        </div>
+        <!--end col-->
+        <div class="col-md-3">
+            <a href="{{ route('wadah-kreasi.index') }}">
+                <div class="card report-card">
+                    <div class="card-body">
+                        <div class="row d-flex justify-content-center">
+                            <div class="col">
+                                <p class="text-dark mb-1 fw-semibold">Wadah Kreasi</p>
+                                <h4 class="font-22 fw-bold">{{ $data['wadah-kreasi'] }}</h4>
+                            </div>
+                            <div class="col-auto align-self-center">
+                                <div
+                                    class="bg-light-alt d-flex justify-content-center align-items-center thumb-md  rounded-circle">
+                                    <i class="ti ti-archive menu-icon"></i><span>
                                 </div>
                             </div>
                         </div>
